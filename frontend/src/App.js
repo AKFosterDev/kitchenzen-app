f5d7b79c5f6546f550a142859db56b738cac61ef
AKFosterDev <akfoster2015@gmail.com>
2021-03-23 13:45:34 -0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import data from './data'
+
+function App() {
+	return (
+		<div className='grid-container'>
+			<header className='row'>
+				<div>
+					<a href='/'>
+						<img className='logo' src='./images/logo.png' alt='KitchZen Logo' />
+					</a>
+				</div>
+				<div>
+					<a className='nav-link' href='/cart'>
+						Cart
+					</a>
+					<a className='nav-link' href='/signin'>
+						Sign In
+					</a>
+				</div>
+			</header>
+			<main>
+				<div className='row center card-container'>
+					{data.products.map(product => (
+						<div key={product._id} className='card'>
+							<a href={`/product/${product._id}`}>
+								<img
+									className='medium'
+									src={product.image}
+									alt={product.name}
+								/>
+							</a>
+							<div className='card-body'>
+								<a href={`/product/${product._id}`}>
+									<h2>{product.name}</h2>
+								</a>
+								<div className='rating'>
+									<span>
+										<i className='fa fa-star'></i>
+										<i className='fa fa-star'></i>
+										<i className='fa fa-star'></i>
+										<i className='fa fa-star'></i>
+										<i className='fa fa-star'></i>
+									</span>
+								</div>
+								<div className='price'>${product.price}</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+			<footer className='row center'>All Rights Reserved</footer>
+		</div>
+	)
+}
+
+export default App
