4ec1a1ca0e638c8fb21e08e94bfd16523bb269c5
AKFosterDev <akfoster2015@gmail.com>
2021-03-24 10:19:30 -0700

    create user schema and router, connect to mongodb and seed sample users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+	users: [
+		{
+			name: 'Amanda',
+			email: 'amanda@email.com',
+			password: bcrypt.hashSync('1234', 8),
+			isAdmin: true,
+		},
+		{
+			name: 'Paul',
+			email: 'paul@email.com',
+			password: bcrypt.hashSync('1234', 8),
+			isAdmin: false,
+		},
+	],
 	products: [
 		{
 			_id: '1',
