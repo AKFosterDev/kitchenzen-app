311f06e63bc5ea60930841c8ade0eb73535f0eca
AKFosterDev <akfoster2015@gmail.com>
2021-03-26 12:26:34 -0700

    add paypal button and initial functionality

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { savePaymentMethod } from '../actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps'
 
 const PaymentMethodScreen = (props) => {
+	const cart = useSelector((state) => state.cart)
+	const { shippingAddress } = cart
+	if (!shippingAddress.address) {
+		props.history.push('/shipping')
+	}
+
 	const [paymentMethod, setPaymentMethod] = useState('PayPal')
 
 	const dispatch = useDispatch()
@@ -43,7 +49,6 @@ const PaymentMethodScreen = (props) => {
 							value='Stripe'
 							name='paymentMethod'
 							required
-							checked
 							onChange={(e) => setPaymentMethod(e.target.value)}
 						/>
 						<label htmlFor='stripe'>Stripe</label>
